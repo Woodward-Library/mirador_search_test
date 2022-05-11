565f411472a0e1c5e0d4d8c545df0bdc9a0d5406
wdrefubc <73660886+wdrefubc@users.noreply.github.com>
2022-05-11 10:25:18 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ Mirador.viewer({
   id: "app",
 
   window: {
-    allowClose: true, // Configure if windows can be closed or not
+    allowClose: false, // Configure if windows can be closed or not
     allowFullscreen: true, // Configure to show a "fullscreen" button in the WindowTopBar
     allowMaximize: false, // Configure if windows can be maximized or not
     allowTopMenuButton: true, // Configure if window view and thumbnail display menu are visible or not
     allowWindowSideBar: true, // Configure if side bar menu is visible or not
     authNewWindowCenter: 'parent', // Configure how to center a new window created by the authentication flow. Options: parent, screen
-    sideBarPanel: 'canvas', // Configure which sidebar is selected by default. Options: info, attribution, canvas, annotations, search
+    sideBarPanel: 'info', // Configure which sidebar is selected by default. Options: info, attribution, canvas, annotations, search
     defaultSidebarPanelHeight: 201,  // Configure default sidebar height in pixels
     defaultSidebarPanelWidth: 235, // Configure default sidebar width in pixels
     defaultView: 'gallery',  // Configure which viewing mode (e.g. single, book, gallery) for windows to be opened in
@@ -26,23 +26,24 @@ Mirador.viewer({
       { key: 'scroll', behaviors: ['continuous'] },
       { key: 'gallery' },
     ],
+    panels: { // Configure which panels are visible in WindowSideBarButtons
+      info: true,
+      attribution: true,
+      canvas: true,
+      annotations: false,
+      search: true,
+      layers: false,
+    },
   },
 
-  panels: { // Configure which panels are visible in WindowSideBarButtons
-    info: true,
-    attribution: true,
-    canvas: true,
-    annotations: false,
-    search: false,
-    layers: false,
-  },
+
 
   thumbnails: {
     preferredFormats: ['jpg', 'png', 'webp', 'tif'],
   },
   thumbnailNavigation: {
-    defaultPosition: 'off', // Which position for the thumbnail navigation to be be displayed. Other possible values are "far-bottom" or "far-right"
-    displaySettings: false, // Display the settings for this in WindowTopMenu
+    defaultPosition: 'far-bottom', // Which position for the thumbnail navigation to be be displayed. Other possible values are "far-bottom" or "far-right"
+    displaySettings: true, // Display the settings for this in WindowTopMenu
     height: 130, // height of entire ThumbnailNavigation area when position is "far-bottom"
     width: 100, // width of one canvas (doubled for book view) in ThumbnailNavigation area when position is "far-right"
   },
@@ -50,9 +51,10 @@ Mirador.viewer({
 
   windows: [
     {
-      "loadedManifest": "https://iiif.lib.harvard.edu/manifests/drs:48309543",
-      "canvasIndex": 2,
+      "loadedManifest": "https://iiif.library.ubc.ca/presentation/cdm.creelman.1-0383264/manifest",
+      "canvasIndex": 0,
       sideBarOpen: false,
+      view:'single',
 
     }
   ],
